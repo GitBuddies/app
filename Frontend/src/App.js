72fa879d0c3a44440dbbe2844595043ef999eea5
jveliquette <juliaveliquette@yahoo.com>
2024-04-11 13:52:51 -0700

    deleted form label

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,7 +11,6 @@ function App() {
       <div className="search-container">
         <div className="search-bar-container">
           <form action="/search" method="GET">
-            {/* <label for="Search">Search:</label> */}
             <input type="text" id="search-bar" name="Q" placeholder="Tell me a little bit about the type of project and language you are interested in viewing..."></input>
             <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
           </form>
