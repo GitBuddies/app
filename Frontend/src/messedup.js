940a83e2e134180f5a76fb1b53a573cb2221ac32
jveliquette <juliaveliquette@yahoo.com>
2024-04-12 15:38:21 -0700

    Revert "Revert "Revert "added gitHubApi, some react components added into app.js, needs styling on submit"""
    This reverts commit 90a0bfd8a51c3c14f7d491ba388068a87b873c68.

--- Frontend/src/messedup.js
+++ Frontend/src/messedup.js
@@ -3,7 +3,7 @@ const OpenAI = require('openai')
 
 const openai = new OpenAI({
   // apiKey: process.env.OPENAI_API_KEY,
-  apiKey: ''
+  apiKey: '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'
 });
 
 const chatCompletion = openai.chat.completions.create({
