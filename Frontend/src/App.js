ea27bf619e5795c275a0c8acd28850114c442671
jveliquette <juliaveliquette@yahoo.com>
2024-04-11 18:02:28 -0700

    added id to header to link to "Back to Top" button

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="app">
-      <header className="app-header">
+      <header className="app-header" id='app-header'>
         <h1>GitBuddies</h1>
         <p>Connecting Coders, Powering Progress</p>
       </header>
@@ -34,7 +35,7 @@ function App() {
         <p>Introducing GitBuddies, a platform harnessing the power of AI to connect developers. No more scrolling and tedious searches – with GitBuddies, finding like-minded collaborators for your next project is effortless. GitBuddies sifts through GitHub, matching users with similar interests, skillsets, and ongoing projects. Whether you're a seasoned developer seeking fresh perspectives or a newcomer looking to dive into the world of open-source, GitBuddies is your ultimate companion for forging meaningful connections and driving innovation forward.</p>
         <p>&copy; 2024 GitBuddies</p>
         <p>Website created by Julia Veliquette</p>
-        <a href="#" class="back-to-top">Back to Top</a>
+        <a href="#app-header" className="back-to-top">Back to Top</a>
 
       </footer>
     </div>
